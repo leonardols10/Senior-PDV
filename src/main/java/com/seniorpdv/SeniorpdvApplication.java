2de1cd1a851ca6ab006546501d97cbef262d92fd
leonardols10 <leonardosilvalima40@gmail.com>
2024-01-30 20:07:41 -0300

    Iniciando o projeto

---
+++ src/main/java/com/seniorpdv/SeniorpdvApplication.java
@@ -0,0 +1,13 @@
+package com.seniorpdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorpdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorpdvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seniorpdv/SeniorpdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seniorpdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorpdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
